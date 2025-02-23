681010cfc46f4ae66af47af10a2172e230e986dd
Artem Buslaev <buslaevartem00@gmail.com>
2025-02-23 17:13:29 +0300

    added validation for inRange value

--- src/numbers/in-range.ts
+++ src/numbers/in-range.ts
@@ -1,12 +1,19 @@
 import { isNotUndefined, isUndefined } from '../base';
+import { isNotNumber } from './is-not-number';
 
 type Interval = { start?: number; end?: number };
 
 export function inRange(
   value: number,
-  { start, end }: Interval,
+  interval: Interval,
   { includeBorders }: { includeBorders?: boolean } = {},
 ): boolean {
+  if (isNotNumber(value)) {
+    throw new Error('Expected value to be number');
+  }
+
+  const [start, end] = [interval?.start, interval?.end];
+
   const greater: (a: number, b: number) => boolean =
     includeBorders ? (a, b) => a >= b : (a, b) => a > b;
   const less: (a: number, b: number) => boolean =
