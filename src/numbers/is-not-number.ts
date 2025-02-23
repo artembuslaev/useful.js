8be3beca35abdac78d18d46607e2da3891293196
Artem Buslaev <buslaevartem00@gmail.com>
2025-02-23 17:14:06 +0300

    fix lint

--- src/numbers/is-not-number.ts
+++ src/numbers/is-not-number.ts
@@ -1,6 +1,6 @@
-import { isNumber } from "./is-number";
+import { isNumber } from './is-number';
 
-type NotNumber<T> = T extends number ? never : T
+type NotNumber<T> = T extends number ? never : T;
 
 export function isNotNumber<T>(value: T): value is NotNumber<T> {
   return !isNumber(value);
